ea513533aa48ffa8c6fc95b50e16363fc5d18760
Allison Demkovich <demkovich.allison@gmail.com>
2022-02-14 15:43:27 -0600

    connect to mysql database

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // add express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // my username
+        user: 'root',
+        //my PW
+        password: 'Mrs.Carlson2023',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // default response for any other request ( not found )
 app.use((req, res) => {
